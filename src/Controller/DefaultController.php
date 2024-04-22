211601968c92c8726ecacfc6a05ac163ff46d024
Pierre Bretéché <pbreteche@dawan.fr>
2024-04-22 15:27:21 +0200

    Request injection in Symfony controllers

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -10,13 +11,19 @@ class DefaultController extends AbstractController
 {
     /**
      * Controller method *must* return a Response instance.
+     * Symfony résout les paramètres demandés par la méthode de contrôleur,
+     * en s'appuyant sur le typage et/ou sur le nommage.
+     * Si la résolution est correctement effectuée, Symfony passe l'argument.
+     * Une erreur est déclenchée sinon.
+     * ⇒ S'appuie sur le système d'injection de dépendance.
      */
     #[Route('/', methods: 'GET')]
-    public function homepage(): Response
+    public function homepage(Request $request): Response
     {
         // helper from AbstractController
         return $this->render('default/homepage.html.twig', [
             'my_var' => 'Hello',
+            'server_name' => $request->server->get('SERVER_NAME'),
         ]);
     }
 }
