26c583c42f8645ab9fa3685fb21b8dd0f4a9c9ce
Pierre Bretéché <pbreteche@dawan.fr>
2024-04-26 14:47:06 +0200

    Exemples cache attribute

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -8,8 +8,8 @@ use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\Cache;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 #[Route('/post')]
 class PostController extends AbstractController
@@ -26,6 +26,7 @@ class PostController extends AbstractController
     }
 
     #[Route('/{id<\d+>}', methods: 'GET')]
+    #[Cache(maxage: 86400, public: true)]
     public function show(
         Post $post,
     ): Response {
