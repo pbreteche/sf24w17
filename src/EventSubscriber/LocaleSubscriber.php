f9a4298017861e2cb56b7955db818052399d63ae
Pierre Bretéché <pbreteche@dawan.fr>
2024-04-26 11:47:57 +0200

    sticky locale session

--- src/EventSubscriber/LocaleSubscriber.php
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -2,16 +2,19 @@
 
 namespace App\EventSubscriber;
 
+use phpDocumentor\Reflection\Types\Self_;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class LocaleSubscriber implements EventSubscriberInterface
 {
+    public const MANAGED_LOCALE = ['fr', 'en_US', 'en'];
+
     public function preferredLanguage(RequestEvent $event): void
     {
         $request = $event->getRequest();
-        $preferredLocale = $request->getPreferredLanguage(['fr', 'en_US', 'en']);
+        $preferredLocale = $request->getPreferredLanguage(static::MANAGED_LOCALE);
         if ($preferredLocale) {
             $request->setLocale($preferredLocale);
         }
@@ -19,6 +22,19 @@ class LocaleSubscriber implements EventSubscriberInterface
 
     public function inSession(RequestEvent $event): void
     {
+        $request = $event->getRequest();
+        $session = $event->getRequest()->getSession();
+        if (
+            ($langQuery = $request->query->getString('lang'))
+            && in_array($langQuery, static::MANAGED_LOCALE)
+        ) {
+            $session->set('locale', $langQuery);
+        }
+
+        if ($sessionLocale = $session->get('locale')) {
+            $request->setLocale($sessionLocale);
+        }
+
         // * gérer si une locale a été stockée en session
         // * proposer dans l'interface utilisateur un moyen de définir sa locale
     }
